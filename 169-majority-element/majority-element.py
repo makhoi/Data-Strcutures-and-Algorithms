db13223be0056c29741ccf10967f97d6de167d3c
Khoi Ma <khoihma@gmail.com>
2025-10-16 18:40:24 -0400

    Time: 6 ms (61.31%) | Memory: 19.3 MB (86.88%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,12 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        occurence = {}
-        for i in range(len(nums)):
-            if nums[i] not in occurence:
-                occurence[nums[i]] = 1
-            else: 
-                occurence[nums[i]] += 1
+        occurences = {}
+        for num in nums: 
+            occurences[num] = occurences.get(num, 0) + 1
+        
 
-        for element, value in occurence.items(): 
-            if value > len(nums)/2:
+        for element, occurence in occurences.items(): 
+            if occurence > len(nums)/2:
                 return element
