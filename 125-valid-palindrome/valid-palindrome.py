76bdc7e1ab2c05d0eb0fdacf539e4d187c76d453
Khoi Ma <khoihma@gmail.com>
2025-11-27 20:22:09 -0500

    Time: 11 ms (36.26%) | Memory: 23.7 MB (5.08%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,20 +2,21 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         normalized_chars = []
 
-        for ch in s: 
-            if ch.isalnum():
+        for ch in s:
+            if ch.isalnum(): 
                 lower_ch = ch.lower()
                 normalized_chars.append(lower_ch)
 
         normalized = ''.join(normalized_chars)
-        
-        reverse_chars = []
 
+        reverse_normalized = []
         for i in range(len(normalized)-1, -1, -1):
-            reverse_chars.append(normalized[i])
-        
-        reverse = ''.join(reverse_chars)
-        
-        if normalized == reverse:
+            reverse_normalized.append(normalized[i])
+
+        reverse = ''.join(reverse_normalized)
+
+        if reverse == normalized:
             return True
         return False
+
+        
