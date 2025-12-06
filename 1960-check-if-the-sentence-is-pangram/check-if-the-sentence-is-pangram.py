f91c1a5d5d3f922c75c95d37249a7dc1bdd4880a
Khoi Ma <khoihma@gmail.com>
2025-12-05 20:20:15 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (30.13%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,39 +1,12 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         alphabet = {
-            'a': 1,
-            'b': 1,
-            'c': 1,
-            'd': 1,
-            'e': 1,
-            'f': 1,
-            'g': 1,
-            'h': 1,
-            'i': 1,
-            'j': 1,
-            'k': 1,
-            'l': 1,
-            'm': 1,
-            'n': 1,
-            'o': 1,
-            'p': 1,
-            'q': 1,
-            'r': 1,
-            's': 1,
-            't': 1,
-            'u': 1,
-            'v': 1,
-            'w': 1,
-            'x': 1,
-            'y': 1,
-            'z': 1
-            }
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z'}
 
-        frequency = {}
-        for ch in sentence: 
-            frequency[ch] = frequency.get(ch, 0) + 1
-        
-        for character in alphabet: 
-            if character not in frequency:
-                return False
-        return True
+        letters = set(sentence)
+
+        if alphabet == letters: 
+            return True
+        return False
