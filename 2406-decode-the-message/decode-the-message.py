d2877009ed6c6cc9b3d1ee68ec49a6f87b78da0b
Khoi Ma <khoihma@gmail.com>
2025-12-20 12:49:35 -0500

    Time: 1 ms (65.44%) | Memory: 17.4 MB (99.71%) - LeetSync

--- 2406-decode-the-message/decode-the-message.py
+++ 2406-decode-the-message/decode-the-message.py
@@ -1,20 +1,42 @@
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        substitution = {}
-        x = ord('a')
+        # ENCODE MESSAGE
 
-        for ch in key: 
-            if ch == ' ':
-                continue
-            if ch not in substitution: 
-                substitution[ch] = chr(x)
-                x += 1
-            
-        result = []
-        for ch in message: 
-            if ch == ' ':
-                result.append(' ')
-            else: 
-                result.append(substitution[ch])
+        # split sentence into single characters 
+        chars = []
+
+        for c in key: 
+            if c != " ":
+                chars.append(c)
         
+        # remove duplicate from chars 
+        unique_chars = []
+        seen = set()
+
+        for c in chars: 
+            if c in seen: 
+                continue
+            
+            seen.add(c)
+            unique_chars.append(c)
+            
+
+        # map each character in unique_chars to alphabet letters
+        chars_map = {}
+        alphabet = ord('a')
+
+        for c in unique_chars: 
+            chars_map[c] = chr(alphabet)
+            alphabet += 1
+
+        # DECODE 
+        result = []
+
+        for c in message: 
+            if c == " ":
+                result.append(" ")
+            else:
+                result.append(chars_map[c])
+
         return ''.join(result)
+        
