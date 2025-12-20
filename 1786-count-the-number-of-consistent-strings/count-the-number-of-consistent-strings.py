e9f31302dc567d7115ab5cf26411251f1fef1e13
Khoi Ma <khoihma@gmail.com>
2025-12-20 13:34:45 -0500

    Time: 233 ms (35.03%) | Memory: 19.1 MB (99.86%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -7,3 +7,7 @@ class Solution:
                 count += 1
         
         return count
+        
+# <= does not mean “smaller size”
+# It means set inclusion
+# Order, size, lexicographic value are irrelevant
