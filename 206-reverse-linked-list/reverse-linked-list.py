0ea89c8226a650dbc10a8d9c6e6d7ef3d7a1c16b
Khoi Ma <khoihma@gmail.com>
2025-12-24 17:49:07 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (94.59%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -1,3 +1,5 @@
+# RECURSION METHOD
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -5,12 +7,14 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = head
+        if head is None or head.next is None:
+            return head
+        
+        reversed_list_head = self.reverseList(head.next)
+        # Get the last node after the list is reversed
+        last_node = head.next
+        # Set the current node as last node's next element
+        last_node.next = head
+        head.next = None
 
-        while curr: 
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        return prev
+        return reversed_list_head
