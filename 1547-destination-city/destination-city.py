5100bedfa765974ccfd853de1d0fb04f7faf1faf
Khoi Ma <khoihma@gmail.com>
2025-12-25 09:28:49 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (88.69%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,10 +1,11 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        arrivals = {}
+        routes = {}
 
-        for cityA, cityB in paths: 
-            arrivals[cityA] = cityB
+        for cityA, cityB in paths:
+            routes[cityA] = cityB
 
-        for cityB in arrivals.values(): 
-            if cityB not in arrivals: 
-                return cityB
+        for arrival in routes.values():
+            if arrival not in routes:
+                return arrival
+        
