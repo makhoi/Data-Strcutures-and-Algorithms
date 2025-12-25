dad6b6d7a0af3165a87dd8918275d5c7e74a59ca
Khoi Ma <khoihma@gmail.com>
2025-12-25 09:26:32 -0500

    Time: 1 ms (87.28%) | Memory: 17.6 MB (83.94%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,10 +1,11 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+
         def checkRow(r):
             seen = set()
             arr = board[r]
             for val in arr:
-                if val == '.':
+                if val == ".":
                     continue
                 if val in seen:
                     return False
@@ -14,7 +15,7 @@ class Solution:
         for r in range(9):
             if not checkRow(r):
                 return False
-
+        
         def checkCol(c):
             seen = set()
             for r in range(9):
@@ -25,26 +26,27 @@ class Solution:
                     return False
                 seen.add(val)
             return True
-
+        
         for c in range(9):
             if not checkCol(c):
                 return False
-
-        def checkBoard(rowIdx, colIdx):
+        
+        def checkBoard(rowIdx,colIdx):
             seen = set()
-            for r in range(rowIdx, rowIdx + 3):
-                for c in range(colIdx, colIdx + 3): 
+            for r in range(rowIdx, rowIdx+3):
+                for c in range(colIdx, colIdx+3):
                     val = board[r][c]
-                    if val == '.':
+                    if val == ".":
                         continue
                     if val in seen:
                         return False
                     seen.add(val)
             return True
 
-        for r in range(0, 9, 3):
-            for c in range(0, 9, 3):
+        for r in range(0,9,3):
+            for c in range(0,9,3):
                 if not checkBoard(r,c):
                     return False
         return True
+
         
