cf4b7473a1ff1405bd651ce1673920ce0e59c550
Khoi Ma <khoihma@gmail.com>
2025-12-26 09:34:34 -0500

    Time: 3 ms (23.19%) | Memory: 17.6 MB (88.37%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.py
+++ 822-unique-morse-code-words/unique-morse-code-words.py
@@ -1,17 +1,17 @@
 class Solution:
     def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        # map alphabet letter with morse code
+        # map alphabet letters with morse code
         alphabet_to_morse = {}
 
         morses = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
 
         alphabet = ord('a')
-        
-        for morse in morses:
+
+        for morse in morses: 
             alphabet_to_morse[chr(alphabet)] = morse
             alphabet += 1
-
-        # Encode 
+        
+        # Encode words with morse code
         encoded_words = []
 
         for word in words: 
@@ -19,12 +19,9 @@ class Solution:
             for ch in word: 
                 encoded.append(alphabet_to_morse[ch])
             encoded_words.append("".join(encoded))
-
-        # Find number of unique morse code
-        unique_code = set()
-
-        for word in encoded_words:
-            unique_code.add(word)
-
-        return len(unique_code)
+        
+        # Add all the encoded words to a set called unique_encoded_words 
+        # return length of that set and we got the result
+        unique_encoded_words = set(encoded_words)
+        return len(unique_encoded_words)
         
