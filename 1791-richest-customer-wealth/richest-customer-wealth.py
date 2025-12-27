e371a870820f23deb153d4aaec12b9f2e8b43463
Khoi Ma <khoihma@gmail.com>
2025-12-27 18:03:35 -0500

    Time: 1 ms (8.65%) | Memory: 17.4 MB (91.19%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.py
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -1,12 +1,14 @@
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
+        left, right = 0, len(accounts[0]) - 1
+        top, bottom = 0, len(accounts) - 1
+
         total = 0
         customers_wealth = []
-
-        for account in accounts:
-            for amount in account:
-                total += amount
+        for r in range(top, bottom + 1):
+            for c in range(left, right + 1):
+                total += accounts[r][c]
             customers_wealth.append(total)
             total = 0
-        
+
         return max(customers_wealth)
