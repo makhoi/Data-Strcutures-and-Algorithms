65c0f1f0f67da265091100c32d8a129488bb5b0e
Khoi Ma <khoihma@gmail.com>
2025-12-28 17:34:49 -0500

    Time: 0 ms (100.00%) | Memory: 17.3 MB (89.34%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,12 +1,13 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        alphabet = {
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-        'u', 'v', 'w', 'x', 'y', 'z'}
+        alphabets = set()
+        for ch in range(ord('a'), ord('z') + 1):
+            alphabets.add(chr(ch))
 
-        letters = set(sentence)
-
-        if alphabet == letters: 
-            return True
-        return False
+        pangram = set()
+        for ch in sentence: 
+            pangram.add(ch)
+        
+        if alphabets != pangram:
+            return False
+        return True
