157eaf5bea29cab5e4e26bbb4384c7c72d670a44
Khoi Ma <khoihma@gmail.com>
2025-12-29 00:31:14 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (84.00%) - LeetSync

--- 2406-decode-the-message/decode-the-message.py
+++ 2406-decode-the-message/decode-the-message.py
@@ -1,21 +1,22 @@
 class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
-        # process given message to character seperated by , and no duplicate 
+        # processing the given key to a array of with no duplicate character 
         key_no_duplicate = []
-        for ch in key: 
+        for ch in key:
             if ch == " ":
                 continue
             if ch in key_no_duplicate:
                 continue
             key_no_duplicate.append(ch)
         
-        # map each character to alphabet letter
+        # map character to alphabet letter
         alphabet_map = {}
         alphabet = ord('a')
-        for ch in key_no_duplicate:
+
+        for ch in key_no_duplicate: 
             alphabet_map[ch] = chr(alphabet)
             alphabet += 1 
-        
+
         # decode message
         decode_message = []
         for ch in message: 
@@ -25,6 +26,3 @@ class Solution:
                 decode_message.append(alphabet_map[ch])
         
         return "".join(decode_message)
-
-
-            
