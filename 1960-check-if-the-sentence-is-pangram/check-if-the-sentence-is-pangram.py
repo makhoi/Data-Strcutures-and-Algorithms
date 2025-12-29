5b142a3d8ae3e86d587314b58cc0b617a71e5750
Khoi Ma <khoihma@gmail.com>
2025-12-28 21:31:04 -0500

    Time: 0 ms (100.00%) | Memory: 17.2 MB (93.69%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,13 +1,18 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
+        # process the given key to separate character with , and no duplicate
+        key_no_duplicate = set()
+        for ch in sentence: 
+            key_no_duplicate.add(ch)
+
+        # set of alphabet letters 
         alphabets = set()
         for ch in range(ord('a'), ord('z') + 1):
             alphabets.add(chr(ch))
 
-        pangram = set()
-        for ch in sentence: 
-            pangram.add(ch)
+        if key_no_duplicate == alphabets: 
+            return True
+        return False
         
-        if alphabets != pangram:
-            return False
-        return True
+        
+        
