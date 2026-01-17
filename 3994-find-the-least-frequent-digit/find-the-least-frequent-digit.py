a050acea0bef756562c449c0252b8cdf050a3117
Khoi Ma <khoihma@gmail.com>
2026-01-17 13:28:40 -0500

    Time: 4 ms (14.21%) | Memory: 19.2 MB (18.53%) - LeetSync

--- 3994-find-the-least-frequent-digit/find-the-least-frequent-digit.py
+++ 3994-find-the-least-frequent-digit/find-the-least-frequent-digit.py
@@ -1,25 +1,23 @@
 class Solution:
     def getLeastFrequentDigit(self, n: int) -> int:
-        '''
-        0. Convert integer to string to be able to traverse
-        1. Create a hashmap called frequency to store frequency of each integer in the number
-        2. Create another hashmap called ... where key is the frequency and value is the list of number with that frequency
-        3. Find the smallest key in the table and return min(value) of that key
-        '''
+        # convert integer n to string to be able to iterate over it
         nums = str(n)
+        # num_to_frequency table to store frequency of each table
+        num_to_frequency = {}
+        for num in nums:
+            num_to_frequency[num] = num_to_frequency.get(num, 0) + 1
 
-        frequencies = {}
-        for num in nums: 
-            frequencies[num] = frequencies.get(num, 0) + 1
-
+        # frequency_to_nums table is basically opposite version of num_to_frequency where key: frequency, value: list of nums
         frequency_to_nums = defaultdict(set)
-        for num, frequency in frequencies.items():
-            frequency_to_nums[frequency].add(num)
-        
-        # find the smallest frequency in frequency_to_nums table:
+        for n,f in num_to_frequency.items():
+            frequency_to_nums[f].add(n)
+
+        # find the smallest frequency from frequency_to_nums table aka the smallest key in frequency_to_nums table
         smallest_frequency = float('inf')
-        for f, num in frequency_to_nums.items():
+        for f in frequency_to_nums.keys():
             if f < smallest_frequency:
                 smallest_frequency = f
 
+        # return the min value of that smallest frequency (convert it back to integer)
         return int(min(frequency_to_nums[smallest_frequency]))
+        
