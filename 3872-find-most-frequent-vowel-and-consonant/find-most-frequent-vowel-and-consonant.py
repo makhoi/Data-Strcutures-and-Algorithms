78a514d40c11661ba8f604dfab44db8ac9518785
Khoi Ma <khoihma@gmail.com>
2026-01-18 23:45:08 -0500

    Time: 1 ms (74.63%) | Memory: 19.4 MB (7.34%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -1,20 +1,16 @@
 class Solution:
     def maxFreqSum(self, s: str) -> int:
-        vowels = {'a', 'e', 'i', 'o','u'}
+        vowels = {'a', 'e', 'i', 'o', 'u'}
 
         vowel_frequency = {}
+        consonant_frequency = {}
         for ch in s:
             if ch in vowels:
                 vowel_frequency[ch] = vowel_frequency.get(ch, 0) + 1
-
-        consonant_frequency = {}
-        for ch in s:
-            if ch not in vowels:
+            else:
                 consonant_frequency[ch] = consonant_frequency.get(ch, 0) + 1
 
         best_vowel_frequency = max(vowel_frequency.values()) if vowel_frequency else 0
         best_consonant_frequency = max(consonant_frequency.values()) if consonant_frequency else 0
 
-        result = best_vowel_frequency + best_consonant_frequency
-
-        return result
+        return best_vowel_frequency + best_consonant_frequency
