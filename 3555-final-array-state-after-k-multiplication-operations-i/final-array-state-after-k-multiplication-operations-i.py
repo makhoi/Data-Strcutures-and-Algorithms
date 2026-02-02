d67b2c97efc0c75d4f7e3fa6e0304cdd71659b09
Khoi Ma <khoihma@gmail.com>
2026-02-01 21:53:45 -0500

    Time: 3 ms (52.30%) | Memory: 19.5 MB (28.29%) - LeetSync

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+        '''
+        1. first determine the smallest value in the array and also keep track of the index 
+        2. replace that value with value*multplier
+        3. reduce k by 1
+        '''
+        # determine the smallest value in the array
+        while k:
+            smallest_value = float('inf')
+            smallest_value_index = 0
+
+            for index, num in enumerate(nums):
+                if num < smallest_value:
+                    smallest_value = num
+                    smallest_value_index = index
+
+            nums[smallest_value_index] = smallest_value*multiplier
+
+            k -= 1
+        
+        return nums
