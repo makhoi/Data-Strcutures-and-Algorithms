e98a2d5006bff7fe1ef60e41a89310bf793e7248
Khoi Ma <khoihma@gmail.com>
2026-02-06 15:10:06 -0500

    Time: 111 ms (22.49%) | Memory: 25.4 MB (19.08%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -1,33 +1,35 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         '''
-        Method 2: Convert 0 to -1 
-
+        When 1s = 0s -> 1s - 0s = 0 -> diff = 0 -> d = 0
         ps[i] = S
-        ps[j,i] = k = 0
-        ps[j-1] = S - k = S - 0 = S
+        ps[j,i] = d = 0
+        ps[j-1] = S - d = S - 0 = S
         '''
         prefix_sum = 0
         prefix_map = {}
-        n = len(nums)
         max_length = 0
+        n = len(nums)
+        diff = 0
 
         for i in range(n):
-            if nums[i] == 0:
-                nums[i] = -1
-            
-            prefix_sum += nums[i]
+            # prefix_sum += nums[i]
 
-            if prefix_sum == 0:
+            if nums[i] == 1:
+                diff += 1
+            else:
+                diff -= 1
+
+            if diff == 0:
                 max_length = max(max_length, i + 1)
 
-            if prefix_sum in prefix_map:
-                jM1 = prefix_map[prefix_sum]
+            if diff in prefix_map:
+                jM1 = prefix_map[diff]
                 j = jM1 + 1
                 l = i - j + 1
                 max_length = max(max_length, l)
 
-            if prefix_sum not in prefix_map:
-                prefix_map[prefix_sum] = i
+            if diff not in prefix_map:
+                prefix_map[diff] = i
 
         return max_length
