fe0bd9e786f591797dfb609dacfc9ad5b86b1fc0
Khoi Ma <khoihma@gmail.com>
2026-02-19 13:28:06 -0500

    Time: 143 ms (33.35%) | Memory: 19.8 MB (39.05%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,22 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         '''
-        stop condition
-        len(window) - most frequency of a character
+        len(current_window) - value of the letter with the most frequency = character needs to be change for it to 
         '''
-        l = 0
-        res = 0
-        character_frequency = {}
-        maxf = 0
+        l = 0 
+        res = 0 
+        count = {}
 
         for r in range(len(s)):
-            character_frequency[s[r]] = character_frequency.get(s[r], 0) + 1
-            maxf = max(maxf, character_frequency[s[r]])
+            count[s[r]] = count.get(s[r], 0) + 1
 
-            while (r-l+1) - maxf > k:
-                character_frequency[s[l]] -= 1
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
                 l += 1
-
+            
             res = max(res, r - l + 1)
         
         return res
