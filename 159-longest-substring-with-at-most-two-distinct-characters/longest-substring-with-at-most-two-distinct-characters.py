0213e650175687ea7fc6cbee66d212de1b060d0a
Khoi Ma <khoihma@gmail.com>
2026-02-26 22:45:05 -0500

    Time: 158 ms (86.23%) | Memory: 19.8 MB (30.75%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
@@ -1,18 +1,20 @@
+# Time: O(n^2)
+# Sliding window type 1 - longest substring / b - add before while loop 
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
         l = 0
-        res = 0
-        character_frequency = {}
+        res = 0 
+        frequency = {}
 
-        for r in range(len(s)):
-            character_frequency[s[r]] = character_frequency.get(s[r],0) + 1
+        for r in range(len(s)): 
+            frequency[s[r]] = frequency.get(s[r], 0) + 1
 
-            while len(character_frequency) == 3: 
-                character_frequency[s[l]] -= 1
-                if character_frequency[s[l]] == 0: 
-                    del character_frequency[s[l]]
+            while len(frequency) > 2:
+                frequency[s[l]] -= 1
+                if frequency[s[l]] == 0:
+                    del frequency[s[l]]
                 l += 1
-            
-            res = max(res, r - l + 1)
 
+            res = max(res, r - l + 1)
+        
         return res
