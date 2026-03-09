5ed24413bf08619e608bec730f1c7c232aa1ace6
Khoi Ma <khoihma@gmail.com>
2026-03-08 22:57:11 -0400

    Time: 0 ms (100.00%) | Memory: 20 MB (42.39%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,12 +1,26 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] == target: 
-                return i
-        else: 
-            nums.append(target)
-            updated = sorted(nums)
-            for j in range(len(updated)):
-                if updated[j] == target: 
-                    return j
+        '''
+        # 1. define search space (left, right)
+        0 -> n
+        # 2. define contidion(mid)
+        # if nums[mid] >= target: 
+            r = mid 
+        # 3. define left meaning after while loop
+        # nums[left] >= target and left is minimum
+        '''
+        n = len(nums)
+        l = 0
+        # r = n - 1
+        r = n # test case phải cover tất cả trường hợp --> 0 -> n -> r = n
+
+        while l < r: 
+            mid = l + (r-l) // 2 # lowerbound 
+
+            if nums[mid] >= target:
+                r = mid
             
+            else: 
+                l = mid + 1
+
+        return l
