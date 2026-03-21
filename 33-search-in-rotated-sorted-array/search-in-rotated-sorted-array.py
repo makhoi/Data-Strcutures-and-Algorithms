bdaacef044095632a6ff713e526e411d2e486edd
Khoi Ma <khoihma@gmail.com>
2026-03-21 02:01:48 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (65.78%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,46 +1,44 @@
 class Solution:
-    def search(self, nums, target):
+    def search(self, nums: List[int], target: int) -> int:
         '''
-        1. lowerbound vs upperbound: no dup -> lowerbound
-        2. search space: 0->n-1
-        3. condition: nums[mid] >= target
-        4. meaning of l: 
-        5. result: 
+        1. lowerbound vs upperbound: no dup -> lowerbound preferred
+        2. search space: 0 -> n-1
+        3. condition to move right: if nums[m] < nums[r]
+        4. meaning of left after exiting the while loop: min l satisfies the condition
+        5. result: if nums[l] == target -> return nums[l] else -1
         '''
+        # find minimum in rotated sorted array
         n = len(nums)
-        l = 0 
-        r = n - 1
-
-        # if nums[0] < nums[n-1]:
-        #     return nums[0]
+        l = 0
+        r = n-1
 
         while l < r: 
             m = l + (r-l)//2
 
-            if nums[m] <= nums[r]: 
+            if nums[m] <= nums[r]: # midpoint belongs to the half right 
                 r = m
             else: 
-                l = m + 1   
-        # chia array thành 2 đoạn:
-        # right: min value -> giá trị nào đó x
-        # left: giá trị y nào đó > max(right) -> max(nums) 
-
-        minIndex = l
+                l = m + 1
         
+        minIndex = l # index of min value
+
+        # the array is divided into 2 parts: 
+        # right part: from minIndex -> n-1
+        # left part: from 0 -> minIndex-1
+
         def binarySearch(left, right, find): 
 
             while left < right: 
-                mid = left + (right - left) // 2
+                mid = left + (right-left)//2
 
-                if nums[mid] >= find: 
+                if nums[mid] >= find:
                     right = mid
                 else: 
                     left = mid + 1
+
             return left if nums[left] == target else -1
 
-        if target <= nums[n-1]: # thuoc right array 
-            return binarySearch(minIndex,n-1,target) 
+        if target <= nums[n-1]:
+            return binarySearch(minIndex, n-1, target)
         else: 
-            # binary search tren left array
-            return binarySearch(0,minIndex-1,target)
-            
+            return binarySearch(0, minIndex-1, target)
