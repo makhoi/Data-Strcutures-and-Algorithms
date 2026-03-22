a171c4b5c245a674b4b5401beb4588fd1d08fbb2
Khoi Ma <khoihma@gmail.com>
2026-03-22 16:21:57 -0400

    Time: 43 ms (67.73%) | Memory: 19 MB (96.91%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -3,15 +3,28 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        l = 0
+        '''
+        0. basic/modified/adhoc: 
+        - modified: array tăng dần và bị biến đổi ở đâu đó
+        - adhoc: phải biến đổi đề thành array () 
+        => Biến đổi thành GGGGBBBB -> FFFFTTTT 
+        => adhoc
+        1. lowerbound vs upperbound: lowerbound but why? there is duplicate 
+        2. search space: 1 -> n
+        3. condition to move right: isBadVersion(m) -> True 
+        4. meaning of left: the first version that isBadVersion(m) returns True 
+        5. result: returns l
+        '''
+        l = 1
         r = n
 
         while l < r: 
             m = l + (r-l)//2
 
-            if isBadVersion(m) == True: 
+            if isBadVersion(m) == True:
                 r = m
             else: 
                 l = m + 1
+        
         return l
         
