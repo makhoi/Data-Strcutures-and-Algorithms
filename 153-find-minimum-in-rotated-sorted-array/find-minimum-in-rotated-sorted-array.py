fbc57a81147b3a9d7b35b2dcb7c16dc57f6a3599
Khoi Ma <khoihma@gmail.com>
2026-03-25 18:29:41 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (52.14%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,25 +1,22 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         '''
-        1. lowerbound vs upperbound: no dup -> lowerbound preferred
-        2. search space: 0 -> n-1
-        3. condition to move right: when midpoint belongs to the right half of the rotated array,
-        we move r pointer to the midpoint -> how to know when midpoint belongs to the right half of the array? when nums[m] < nums[r]
-        Hence: if nums[m] < nums[r]: r = m
-        4. meaning of left after exiting the while loop: min left satisfies the condition nums[l] < nums[r]
-        5. result: min element -> nums[l]
+        0. which type: basic/modified/adhoc? array gradually increasing and being modifed -> modified
+        1. lowerbound vs upperbound? find min -> find first appearance of target? ->  lowerbound
+        2. search space: index ranges from [0, n-1]
+        3. condition to move right: if mid belongs to the second half -> if nums[mid] <= nums[n-1]
+        4. meaning of left after exiting the while loop: min left satisfies the fcondtion if nums[left] <= nums[n-1] -> min value 
+        5. result: return nums[left]
         '''
         n = len(nums)
-        l = 0
-        r = n-1
+        left = 0
+        right = n - 1
 
-        while l < r: 
-            m = l + (r-l)//2
+        while left < right: 
+            mid = left + (right - left)//2
 
-            if nums[m] <= nums[r]: # midpoint is in the half right -> move r to midpoint
-                r = m
-            else: # midpoint is in the half left -> move l to midpoint
-                # l = m
-                l = m + 1 # I forgot why is this m + 1 here? tạm thời cứ mặc định là vậy, đợi review dạng 1 ắt sẽ rõ
-
-        return nums[l]
+            if nums[mid] <= nums[n-1]:
+                right = mid
+            else:
+                left = mid + 1
+        return nums[left]
