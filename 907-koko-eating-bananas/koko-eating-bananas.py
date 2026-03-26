37913fbba71388e7dfaf90856ef542b281d6566f
Khoi Ma <khoihma@gmail.com>
2026-03-25 20:50:43 -0400

    Time: 183 ms (34.75%) | Memory: 20.6 MB (66.68%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,55 +1,37 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         '''
-        eating speed = k = # of banana/hour
-        def canEat(k)
-
-        piles = [3,6,7,11], h = 8
-        canEat(1) -> F
-        canEat(2) -> F
-        canEat(3) -> F
-        canEat(4) -> T
-        canEat(5) -> T
-        canEat(6) -> T
-        --> find the first k that makes canEat(k) returns TRUE
-        
-        0. binary search: 
-        tính tăng dần ?: index or k tăng dần? yes?
-        tính liên tiếp: nếu canEat(k) là True thì tất cả các canEat(k) về sau đều là True, k tăng dần 
-        -> binary search problem: yes
-        1. basic/modified/adhoc: biến đổi từ đề thành array FFTTT -> adhoc
-        2. lowerbound vs upperbound: lowerbound 
-        3. search space: 1 -> max(piles)
-        4. condition to move right: 
-
-        ----------------------------------
-        l               m                r
-                        m -> T: move r to m
-                        m -> False: move l to m + 1
-        5. result: return l 
+        00. Determine it is adhoc problem
+        0. Understand the problem
+        def can_eat(k):
+            # function returns True if with k speed KoKo can finish all all the banana until the guard is back 
+            # first write a function to calculate how many hours KoKo need to take to finish piles of banana with k speed 
+            # FFFFFTTTTTTTTTT 
+        1. lowerbound vs upperbound: find first T -> first appearance of the target -> lowerbound 
+        2. search space: 1 -> max(piles)
+        3. condition to move right: if can_eat(mid) == True 
+        4. meaning of left: first k that can_eat(left) == True
+        5. return left -> k
         '''
-
-        def canEat(k): 
+        def can_eat(k):
             total_hours = 0
-            '''
-            takes in k - eating speed -> how to find total hours?
-            lets say k = 3 -> total hours = 1 + 2 + 3 + 4 = 10
-                                          = (piles[0]/k + 1 if piles[0]%k != 0) + (piles[1]/k + 1 if piles[1]%k != 0) + (piles[2]/k + 1 if piles[2]%k != 0) 
-            '''
-            for i in range(len(piles)): 
-                total_hours += piles[i]//k + (1 if piles[i]%k != 0 else 0)
-            
-            return total_hours <= h # if total_hours less or equal to h return True else False
+            for pile in piles: 
+                total_hours += pile // k 
+                if pile%k != 0:
+                    total_hours += 1
+            return total_hours <= h
 
-        l = 1
-        r = max(piles)
+        '''
+        Now we got function that can take in speed and return FFFTTTTT -> find the first T
+        '''
+        left = 1
+        right = max(piles)
 
-        while l < r: 
-            m = l + (r-l)//2
+        while left < right: 
+            mid = left + (right - left)//2
 
-            if canEat(m) == True:
-                r = m
+            if can_eat(mid) == True: 
+                right = mid
             else: 
-                l = m + 1
-        
-        return l
+                left = mid + 1
+        return left
