0ff51b0bdb2d491a841debfd50bae0789f7419d2
Khoi Ma <khoihma@gmail.com>
2026-03-25 21:28:36 -0400

    Time: 0 ms (100.00%) | Memory: 26.9 MB (78.65%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,26 +1,20 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        '''
-        1. lowerbound vs upperbound: dup -> upperbound
-        2. search space: 0 -> n-1
-        3. condition to move right: leftSize % 2 == 1
-        4. meaning of left after exiting the while loop: 
-        5. result: nums[l]
-        '''
         n = len(nums)
-        l = 0
-        r = n-1
+        left = 0
+        right = n - 1
 
-        while l < r:
-            m = l + (r-l)//2
+        while left < right: 
+            mid = left + (right - left)//2
 
-            leftSize = m-1 if nums[m-1] == nums[m] else m
-            if leftSize % 2 == 1:
-                r = m
-            else: 
-                l = m + 1
-                
-        if l % 2 == 1:
-            return nums[l-1]
-        else: 
-            return nums[l]
+            left_size = mid - 1 if nums[mid] == nums[mid-1] else mid
+
+            if left_size % 2 == 1:
+                right = mid
+            else:
+                left = mid + 1
+
+        if left % 2 == 1: 
+            return nums[left-1]
+        else:
+            return nums[left]
