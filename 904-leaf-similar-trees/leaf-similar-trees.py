b9affbf734e305b60a259db191fc28c81fc4ba2e
Khoi Ma <khoihma@gmail.com>
2026-04-03 17:46:17 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.58%) - LeetSync

--- 904-leaf-similar-trees/leaf-similar-trees.py
+++ 904-leaf-similar-trees/leaf-similar-trees.py
@@ -1,30 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-
-        def get_leaves(node): 
-            leaves = []
-
-            def dfs(n):
-                if not n: 
-                    return 
-                
-                if not n.left and not n.right: 
-                    leaves.append(n.val)
-                
-                dfs(n.left)
-                dfs(n.right)
-
-                return 
+        
+        def dfs(node, res):
+            if not node:
+                return
             
-            dfs(node)
-            return leaves 
-        
-        return get_leaves(root1) == get_leaves(root2)
+            if not node.left and not node.right:
+                res.append(node.val)
 
+            dfs(node.left, res)
+            dfs(node.right, res)
+
+            return
         
+        res1 = []
+        dfs(root1, res1)
+
+        res2 = []
+        dfs(root2, res2)
+
+        return res1 == res2
