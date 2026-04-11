0e2eb2266fe1f0ae9239942fa1cb6badddfb0eb8
Khoi Ma <khoihma@gmail.com>
2026-04-11 15:18:18 -0400

    Time: 0 ms (100.00%) | Memory: 20 MB (25.27%) - LeetSync

--- 1609-find-all-the-lonely-nodes/find-all-the-lonely-nodes.py
+++ 1609-find-all-the-lonely-nodes/find-all-the-lonely-nodes.py
@@ -1,21 +1,22 @@
 class Solution:
     def getLonelyNodes(self, root: Optional[TreeNode]) -> List[int]:
+        
         res = []
 
         def dfs(node):
             if not node:
                 return 
-
-            dfs(node.left)
-            dfs(node.right)
+            
+            if node.left and not node.right:
+                res.append(node.left.val)
 
             if not node.left and node.right:
                 res.append(node.right.val)
 
-            if not node.right and node.left:
-                res.append(node.left.val)
+            dfs(node.left)
+            dfs(node.right)
 
-            return 
+            return
         
         dfs(root)
         return res
