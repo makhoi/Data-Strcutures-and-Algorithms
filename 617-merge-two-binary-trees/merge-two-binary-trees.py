807e84f36f5ac10f425df21e65122adb81cb3bb2
Khoi Ma <khoihma@gmail.com>
2026-04-12 14:03:59 -0400

    Time: 5 ms (19.50%) | Memory: 19.6 MB (22.76%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -1,3 +1,4 @@
+# Create a new tree method
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root1 and not root2:
