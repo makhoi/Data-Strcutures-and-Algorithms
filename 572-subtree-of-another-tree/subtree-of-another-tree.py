e671f902c12a7e1da6a8a167427336ba83b93fd3
Khoi Ma <khoihma@gmail.com>
2026-04-12 17:19:11 -0400

    Time: 15 ms (92.97%) | Memory: 19.4 MB (89.03%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # perform inorder traversal on both tree first
+        def dfs(node, arr):
+            if not node:
+                arr.append(None)
+            else:
+                dfs(node.left, arr)
+                dfs(node.right, arr)
+                arr.append(node.val)
+
+            return
+        
+        arr1 = []
+        dfs(root, arr1)
+
+        arr2 = []
+        dfs(subRoot, arr2)
+
+        # find if there is subRoot in original tree
+        n = len(arr1)
+        m = len(arr2)
+
+        for i in range(n-m+1):
+            if arr1[i:i+m] == arr2:
+                return True
+        
+        return False
