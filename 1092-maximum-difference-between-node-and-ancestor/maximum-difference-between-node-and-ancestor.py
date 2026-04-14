3eff79b6b24ebda930f84296228496bbcccb5439
Khoi Ma <khoihma@gmail.com>
2026-04-14 14:38:33 -0400

    Time: 0 ms (100.00%) | Memory: 20.9 MB (55.56%) - LeetSync

---
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, curr_min, curr_max):
+            if not node:
+                return curr_max - curr_min
+            
+            curr_min = min(curr_min, node.val)
+            curr_max = max(curr_max, node.val)
+
+            left_max_diff = dfs(node.left, curr_min, curr_max)
+            right_max_diff = dfs(node.right, curr_min, curr_max)
+
+            return max(left_max_diff, right_max_diff)
+        
+        return dfs(root, root.val, root.val)
+
+
+        
