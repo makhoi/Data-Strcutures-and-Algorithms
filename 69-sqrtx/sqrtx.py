5ff1aa29d011d37090b2f5af8c3150ff2f95e551
Khoi Ma <khoihma@gmail.com>
2026-04-17 13:47:58 -0400

    Time: 3 ms (67.46%) | Memory: 19.2 MB (66.20%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,43 +1,24 @@
-'''
-Taị sao xài binary search?
-1. Search space tăng dần 0 -> x
-2. Tính liên tiếp: 
-a) mid^2 < x^2 => (any number < mid)^2 < x^2
-b) mid^2 > x^2 => (any number > mid)^2 > x^2
-'''
 class Solution:
     def mySqrt(self, x: int) -> int:
         '''
-        # 1. decide lowerbound vs upperbound
-        search space without duplicate --> any works but prefer lower bound because its ezier
-        search space with duplicates and find the latest target --> only upperbound works
-        ==> no dup --> lowerbound
-
-        # 2. define search space (left, right)
-        0 -> x -> 0 -> n + 1
-        # 3. define contidion(mid)
-        if nums[mid]**2 >= x: 
-            l = mid
-        # 4. define left meaning after while loop
-        min l satisfies l^2 >= x 
-        # 5. what to return
-        return l 
+        lowerbound vs upperbound: round down -> first appearance -> lowerbound
+        search space: 0 -> x
+        condition to move right: mid ** 2 >= x
+        meaning of left: min left satisfies condition
+        return mid
         '''
-        # nums = []
-        # for num in range(x):
-        #     nums.append(num)
+        left = 0
+        right = x
 
-        l = 0
-        r = x
-
-        while l < r: 
-            mid = l + (r-l) // 2
+        while left < right:
+            mid = left + (right-left)//2
 
             if mid**2 >= x: 
-                r = mid
-            else: 
-                l = mid + 1
-        if l ** 2 == x:
-            return l
-        elif l ** 2 > x:
-            return l - 1
+                right = mid
+            else:
+                left = mid + 1
+        
+        if left ** 2 == x:
+            return left
+        else:
+            return left - 1
