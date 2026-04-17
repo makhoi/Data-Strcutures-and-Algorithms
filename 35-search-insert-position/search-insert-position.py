c5deaa45d01c3216a648762e383cbdd35fc57126
Khoi Ma <khoihma@gmail.com>
2026-04-17 11:59:14 -0400

    Time: 0 ms (100.00%) | Memory: 19.8 MB (85.48%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,22 +1,15 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        '''
-        0. Binary search? Yes
-        1. lowerbound vs upperbound? no dup -> lowerbound
-        2. search space: 0 -> n
-        3. condition: if nums[mid] >= target 
-        4. meaning: min l satisfies condition
-        5. result: l
-        '''
         n = len(nums)
-        l = 0
-        r = n
+        left = 0
+        right = n
 
-        while l < r: 
-            m = l + (r-l)//2
+        while left < right:
+            mid = left + (right-left)//2
 
-            if nums[m] >= target: 
-                r = m
-            else: 
-                l = m + 1
-        return l
+            if target <= nums[mid]:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
