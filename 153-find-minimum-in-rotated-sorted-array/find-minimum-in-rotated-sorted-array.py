9fd9a0cb9e94a5a284203fe99c1421e5158cc79f
Khoi Ma <khoihma@gmail.com>
2026-04-18 18:11:45 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (33.26%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -1,22 +1,15 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        '''
-        0. which type: basic/modified/adhoc? array gradually increasing and being modifed -> modified
-        1. lowerbound vs upperbound? find min -> find first appearance of target? ->  lowerbound
-        2. search space: index ranges from [0, n-1]
-        3. condition to move right: if mid belongs to the second half -> if nums[mid] <= nums[n-1]
-        4. meaning of left after exiting the while loop: min left satisfies the fcondtion if nums[left] <= nums[n-1] -> min value 
-        5. result: return nums[left]
-        '''
         n = len(nums)
         left = 0
         right = n - 1
 
-        while left < right: 
-            mid = left + (right - left)//2
+        while left < right:
+            mid = left + (right-left)//2
 
-            if nums[mid] <= nums[n-1]:
+            if nums[mid] <= nums[right]:
                 right = mid
             else:
                 left = mid + 1
+
         return nums[left]
