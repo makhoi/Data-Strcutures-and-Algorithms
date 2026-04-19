78f452030d83b6c8dc3b09e62b3a29edac8ccaa5
Khoi Ma <khoihma@gmail.com>
2026-04-18 23:04:10 -0400

    Time: 195 ms (21.78%) | Memory: 20.5 MB (97.12%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,37 +1,29 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         '''
-        00. Determine it is adhoc problem
-        0. Understand the problem
-        def can_eat(k):
-            # function returns True if with k speed KoKo can finish all all the banana until the guard is back 
-            # first write a function to calculate how many hours KoKo need to take to finish piles of banana with k speed 
-            # FFFFFTTTTTTTTTT 
-        1. lowerbound vs upperbound: find first T -> first appearance of the target -> lowerbound 
-        2. search space: 1 -> max(piles)
-        3. condition to move right: if can_eat(mid) == True 
-        4. meaning of left: first k that can_eat(left) == True
-        5. return left -> k
+        define a function to return True if koko can eat all the piles within h hours
+                                    False if koko can not
+        calculate total hours koko eat with that speed
+        if total <= h: return true
         '''
-        def can_eat(k):
-            total_hours = 0
-            for pile in piles: 
-                total_hours += pile // k 
-                if pile%k != 0:
-                    total_hours += 1
-            return total_hours <= h
+        def canEat(speed):
+            total = 0
 
-        '''
-        Now we got function that can take in speed and return FFFTTTTT -> find the first T
-        '''
+            for pile in piles:
+                total += pile // speed
+                if pile%speed != 0:
+                    total += 1
+            return total <= h
+
         left = 1
         right = max(piles)
 
-        while left < right: 
-            mid = left + (right - left)//2
+        while left < right:
+            mid = left + (right-left)//2
 
-            if can_eat(mid) == True: 
+            if canEat(mid) == True:
                 right = mid
-            else: 
+            else:
                 left = mid + 1
+        
         return left
