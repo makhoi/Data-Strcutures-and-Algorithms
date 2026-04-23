35f8e8867f3a55bcfa1567d51fe2f60c5ae979ae
Khoi Ma <khoihma@gmail.com>
2026-04-23 19:07:05 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (42.97%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,40 +1,33 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        '''
-        binary search: 
-        - Dạng tìm element - no dup - either upperbound or lowerbound is fine
-        - Modified binary search
-        lowerbound vs upperbound: lowerbound
-        search space: 0 -> n-1
-        condition to move right: if target <= nums[mid]
-        return nums[left] if nums[left] == target else -1
-        '''
+            
+        # first find index of min value
         n = len(nums)
         left = 0
-        right = n-1
+        right = n - 1
 
         while left < right:
-            mid = left + (right-left)//2
+            mid = left + (right - left) // 2
 
             if nums[mid] <= nums[right]:
                 right = mid
             else:
                 left = mid + 1
         
-        index_min_value = left
+        minIndex = left
 
+        # define binary search function
         def binarySearch(start, end, find):
             while start < end:
-                mid = start + (end-start)//2
+                middle = start + (end-start)//2
 
-                if find <= nums[mid]:
-                    end = mid
+                if find <= nums[middle]:
+                    end = middle
                 else:
-                    start = mid + 1
-
-            return -1 if nums[start] != find else start
+                    start = middle + 1
+            return start if nums[start] == find else -1
 
         if target <= nums[n-1]:
-            return binarySearch(index_min_value, n-1, target)
+            return binarySearch(minIndex, n-1, target)
         else:
-            return binarySearch(0, index_min_value-1, target)
+            return binarySearch(0, minIndex-1, target)
