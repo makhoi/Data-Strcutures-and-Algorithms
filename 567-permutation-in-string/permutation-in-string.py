e43eeec3de89c2328bc0e1267330de449a0b4a84
Khoi Ma <khoihma@gmail.com>
2026-04-25 17:29:20 -0400

    Time: 22 ms (50.14%) | Memory: 19.3 MB (87.83%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,22 +3,20 @@ class Solution:
         s1_count = {}
         for ch in s1:
             s1_count[ch] = s1_count.get(ch, 0) + 1
-
-        l = 0
-        s2_count = {}
-
-        for r in range(len(s2)):
-            s2_count[s2[r]] = s2_count.get(s2[r], 0) + 1
-
-            while r - l + 1 > len(s1):
-                s2_count[s2[l]] -= 1
-                if s2_count[s2[l]] == 0:
-                    del s2_count[s2[l]]
-                l += 1
-
-            if r - l + 1 == len(s1):
-                if s2_count == s1_count:
-                    return True
-        return False
-
         
+        s2_count = {}
+        left = 0
+
+        for right in range(len(s2)):
+            s2_count[s2[right]] = s2_count.get(s2[right], 0) + 1
+
+            while right - left + 1 > len(s1):
+                s2_count[s2[left]] -= 1
+                if s2_count[s2[left]] == 0:
+                    del s2_count[s2[left]]
+                left += 1
+            
+            if s1_count == s2_count:
+                return True
+
+        return False
