4cc8b24378fbe002e7091548f00e19397429bd94
Khoi Ma <khoihma@gmail.com>
2026-04-26 13:39:51 -0400

    Time: 20 ms (19.10%) | Memory: 19.3 MB (71.12%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,23 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        '''
-        1. Iterate over every element in the input to expanding the window: using a set to record every character we traversed
-            1.1. Condition to stop expanding: we check if that character is already exist in the set. If yes:
-                1.1.1. Remove s[left] until that deduplicating character no longer in the set
-                1.1.2. Contracting the window
-            1.2. Proceed with current valid window: since we are looking for longest substring, we are going to use function max() to compare the current result and length of current substring
-        '''
         res = 0
+        character_count = {}
         left = 0
-        seen = set()
 
         for right in range(len(s)):
 
-            while s[right] in seen:
-                seen.remove(s[left])
+            while character_count.get(s[right], 0) == 1:
+                character_count[s[left]] -= 1
+                if character_count[s[left]] == 0:
+                    del character_count[s[left]]
                 left += 1
-            
-            seen.add(s[right])
+
+            character_count[s[right]] = character_count.get(s[right], 0) + 1
 
             res = max(res, right - left + 1)
-        return res 
+            
+        return res
