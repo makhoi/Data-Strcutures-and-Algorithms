aa7e9be22ab1378a5dcc57ab5ba788106b95ab0b
Khoi Ma <khoihma@gmail.com>
2026-04-26 15:19:26 -0400

    Time: 75 ms (28.97%) | Memory: 22 MB (82.81%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -1,64 +1,91 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         '''
-        l
+
+
+           l
                r
         10,5,2,6
+      
+        k = 100
 
-        product = 1
+
         count = 0
+      
+        product = 1
 
-        Start: 
+
+        Start:
         l = 0
         r = 0
-        [10]
-        product = 1*10 = 10 < k 
+        product = 1*10 = 10 < 100
         count = 1
+        [10]
 
-        add 1 element -> len of window = 1 -> count + 1
 
         l = 0
         r = 1
-        [10, 5]
-        product = 10*5 = 50 < k 
-        count = 1 + 2 = 3
+        product = 10*5 = 50 < 100
+        count = 2
+        [10,5] => [5] [10,5]
 
-        add 1 element - len of window = 2 -> count + 2
 
         l = 0
         r = 2
-        [10, 5, 2]
-        product = 50*2 = k -> shrink from left
-        l = 1
-        r = 2
-        [5,2]
-        product = 100/10 = 10
-        count = 3 + 2 = 5
+        [10,5,2] -> invalid subarray
+        product = 50*2 = 100 = 100 (invalid)
 
-        add 1 element - len of window = 2 -> count + 2
 
+        product = current_product/nums[l] = 100/10 = 10 (thoa dk)
+        l += 1 = 1
+        [5,2] => [2] [5,2]
+        count = 3
+      
         l = 1
         r = 3
-        [5,2,6]
-        product = 10*6 = 60 < k
-        count = 5 + 3 = 8
+        [5,2,6] =>  [6] [2,6] [5,2,6]
+        product = 10*6 = 60 (Thoa dk)
+        count = 4
 
-        add 1 element - len of window = 3 -> count +3
 
-        pattern:
-        --> count += len of the window = r - l + 1
         '''
         l = 0
-        product = 1
         count = 0
+        product = 1
+        '''
+        1,2,3
+        k = 0
+
+
+        l = 0
+        r = 0
+        product = 1*1 = 1 >= k = 0 -> enter while
+        product = 1/1 = 1
+        count = 0
+
+
+        l = 1
+        r =
+
+
+
+
+        '''
+
 
         for r in range(len(nums)):
             product *= nums[r]
 
+
             while product >= k and l <= r:
-                product = product // nums[l]
+                product = product / nums[l]
                 l += 1
-            
+
+
             count += r - l + 1
-        
+
+
         return count
+
+
+
