c1076f1f857dfea16920e4f3039788e58af4a28e
Khoi Ma <khoihma@gmail.com>
2026-04-27 21:56:08 -0400

    Time: 5 ms (38.13%) | Memory: 19.6 MB (10.82%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,51 +1,51 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        def checkRow(r):
+
+        ROWS = len(board)
+        COLS = len(board[0])
+
+        def isValidRow(rowIndex):
             seen = set()
-            arr = board[r]
-            for val in arr:
-                if val == '.':
+            for c in range(COLS):
+                if board[rowIndex][c] == '.':
                     continue
-                if val in seen:
+                if board[rowIndex][c] in seen:
                     return False
-                seen.add(val)
+                seen.add(board[rowIndex][c])
             return True
         
-        for r in range(9):
-            if not checkRow(r):
+        for row in range(ROWS):
+            if not isValidRow(row):
                 return False
 
-        def checkCol(c):
+        def isValidColumn(colIndex):
             seen = set()
-            for r in range(9):
-                val = board[r][c]
-                if val == '.':
+            for r in range(ROWS):
+                if board[r][colIndex] == '.':
                     continue
-                if val in seen:
+                if board[r][colIndex] in seen:
                     return False
-                seen.add(val)
+                seen.add(board[r][colIndex])
             return True
-
-        for c in range(9):
-            if not checkCol(c):
+        
+        for col in range(COLS):
+            if not isValidColumn(col):
                 return False
 
-        def checkBoard(rowIdx, colIdx):
+        def isValidSquare(rowIndex, colIndex): 
             seen = set()
-            for r in range(rowIdx, rowIdx + 3):
-                for c in range(colIdx, colIdx + 3):
-                    val = board[r][c]
-                    if val == '.':
+            for r in range(rowIndex, rowIndex + 3):
+                for c in range(colIndex, colIndex + 3):
+                    if board[r][c] == '.':
                         continue
-                    if val in seen:
+                    if board[r][c] in seen:
                         return False
-                    seen.add(val)
+                    seen.add(board[r][c])
             return True
-
+        
         for r in range(0,9,3):
             for c in range(0,9,3):
-                if not checkBoard(r,c):
+                if not isValidSquare(r,c):
                     return False
+
         return True
-        
