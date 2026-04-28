42aa5917d7e41d1675c9993bf14f3ea0d6653c95
Khoi Ma <khoihma@gmail.com>
2026-04-27 23:27:37 -0400

    Time: 315 ms (5.39%) | Memory: 23.5 MB (5.06%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,27 +1,34 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Iterate over every elements in the matrix
+        -> determine the coordinates of all the element which value is 0
+        lets say it is [a,b] -> convert every element in that row (if not 0 to .)
+        have a list to store coordinations of all the 0s
+        then comeback and convert all the . into 0
+        """
+
         ROWS = len(matrix)
         COLS = len(matrix[0])
 
-        def setRow(r):
-            for c in range(COLS):
-                if matrix[r][c] != 0:
-                    matrix[r][c] = float('inf')
-        
-        def setColumn(c):
-            for r in range(ROWS):
-                if matrix[r][c] != 0:
-                    matrix[r][c] = float('inf')
+        zeros = []
 
         for r in range(ROWS):
             for c in range(COLS):
                 if matrix[r][c] == 0:
-                    setRow(r)
-                    setColumn(c)
+                    zeros.append([r,c])
         
+        for r,c in zeros:
+            for col in range(COLS):
+                if matrix[r][col] != 0 and matrix[r][col] != float('inf'):
+                    matrix[r][col] = float('inf')
+
+            for row in range(ROWS):
+                if matrix[row][c] != 0 and matrix[row][c] != float('inf'):
+                    matrix[row][c] = float('inf')
+
         for r in range(ROWS):
             for c in range(COLS):
                 if matrix[r][c] == float('inf'):
                     matrix[r][c] = 0
 
-        
