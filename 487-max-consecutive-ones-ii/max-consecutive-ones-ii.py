80e51f39a2464b6f6cb0289d6da844155ff17e82
Khoi Ma <khoihma@gmail.com>
2026-04-29 17:41:12 -0400

    Time: 31 ms (47.15%) | Memory: 19.7 MB (36.26%) - LeetSync

--- 487-max-consecutive-ones-ii/max-consecutive-ones-ii.py
+++ 487-max-consecutive-ones-ii/max-consecutive-ones-ii.py
@@ -1,18 +1,22 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        '''
+        find longest subarray with no more than 1 zero
+        -> condition to stop expanding the window: frequency of 0 == 2
+        '''
         res = 0
-        left = 0
         num0 = 0
+        left = 0 
 
         for right in range(len(nums)):
+            if nums[right] == 0:
+                num0 += 1
 
-            while num0 == 1 and nums[right] == 0:
+            while num0 == 2:
                 if nums[left] == 0:
                     num0 -= 1
                 left += 1
+            
             res = max(res, right - left + 1)
 
-            if nums[right] == 0:
-                num0 += 1
-                
         return res
