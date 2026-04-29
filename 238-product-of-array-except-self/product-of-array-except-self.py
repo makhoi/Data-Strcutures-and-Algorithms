6ac374c3c22aaaac4b7544002206a73c99cf31cb
Khoi Ma <khoihma@gmail.com>
2026-04-29 14:54:24 -0400

    Time: 47 ms (8.66%) | Memory: 26.9 MB (31.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,18 +1,46 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        '''
+        cummulative_product[i] = product[len(nums)]/cummulative_product[i-1]
+        if frequency of 0 = 1: every product s 0 except at index of value 0
+        if frequency of 0 > 1: every thing is 0
+        '''
+        number_frequency = {}
+        for num in nums:
+            number_frequency[num] = number_frequency.get(num, 0) + 1
+        
         n = len(nums)
-        res = [0]*n
+        cummulative_product = [0]*n
+        cummulative_product[0] = nums[0]
+        for i in range(1, n):
+            cummulative_product[i] = cummulative_product[i-1]*nums[i]
+        product = cummulative_product[n-1]
 
-        # left pass
-        left_product = 1
-        for i in range(n):
-            res[i] = left_product
-            left_product *= nums[i]
+        def rightProduct(index):
+            return product // cummulative_product[index]
 
-        # right pass
-        right_product = 1
-        for i in range(n-1, -1, -1):
-            res[i] *= right_product
-            right_product *= nums[i]
+        def leftProduct(index):
+            return cummulative_product[index-1] if index != 0 else 1
 
-        return res       
+
+        res = []
+        if number_frequency.get(0,0) == 0:
+            for i in range(n):
+                res.append(rightProduct(i)*leftProduct(i))
+        
+        if number_frequency.get(0,0) == 1:
+            non_zero_product = 1
+            for num in nums:
+                if num != 0:
+                    non_zero_product *= num
+
+            for i in range(n):
+                if nums[i] == 0:
+                    res.append(non_zero_product)
+                else:
+                    res.append(0)
+
+        if number_frequency.get(0,0) > 1:
+            res = [0]*n
+        
+        return res
