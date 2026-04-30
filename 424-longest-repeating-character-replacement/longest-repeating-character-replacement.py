8fbc1191857711201b9d80c0ef0937281df73aeb
Khoi Ma <khoihma@gmail.com>
2026-04-30 17:30:20 -0400

    Time: 170 ms (15.56%) | Memory: 19.6 MB (51.59%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -6,10 +6,15 @@ class Solution:
 
         for right in range(len(s)):
             character_frequency[s[right]] = character_frequency.get(s[right], 0) + 1
+            max_character = max(character_frequency, key=character_frequency.get)
+            most_count = character_frequency[max_character]
 
-            while (right - left + 1) - max(character_frequency.values()) > k:
+            while (right - left + 1) - most_count > k:
                 character_frequency[s[left]] -= 1
+                if character_frequency[s[left]] == 0:
+                    del character_frequency[s[left]]
                 left += 1
-            res = max(res, right - left + 1)
 
+            res = max(res, right - left + 1)
+        
         return res
