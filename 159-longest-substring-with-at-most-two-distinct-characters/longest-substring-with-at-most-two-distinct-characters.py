562c7ca2b6795b8be569df315be7eee2f6c01952
Khoi Ma <khoihma@gmail.com>
2026-04-30 16:56:04 -0400

    Time: 171 ms (60.64%) | Memory: 19.8 MB (33.84%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
@@ -5,13 +5,15 @@ class Solution:
         left = 0
 
         for right in range(len(s)):
-            character_frequency[s[right]] = character_frequency.get(s[right], 0) + 1
 
-            while len(character_frequency) == 3:
+            while len(character_frequency) == 2 and s[right] not in character_frequency:
                 character_frequency[s[left]] -= 1
                 if character_frequency[s[left]] == 0:
                     del character_frequency[s[left]]
                 left += 1
+            
+            character_frequency[s[right]] = character_frequency.get(s[right], 0) + 1
+
             res = max(res, right - left + 1)
-        
+            
         return res
