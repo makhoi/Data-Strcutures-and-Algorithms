34c0b0f14519d382b5e7a3b7b14cd56127521d26
Khoi Ma <khoihma@gmail.com>
2026-04-30 18:05:01 -0400

    Time: 64 ms (48.36%) | Memory: 22 MB (96.11%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.py
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.py
@@ -4,13 +4,13 @@ class Solution:
         product = 1
         left = 0
 
+
         for right in range(len(nums)):
             product *= nums[right]
 
             while product >= k and left <= right:
-                product = product / nums[left]
+                product = product/nums[left]
                 left += 1
-            
             count += right - left + 1
-            
+
         return count
