dd3c5cab02040c1e37f08a4576dd195334c2b91e
Khoi Ma <khoihma@gmail.com>
2026-05-02 14:34:57 -0400

    Time: 3 ms (76.20%) | Memory: 19.4 MB (35.90%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,24 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-
         ROWS = len(board)
-        COLS = len(board[0])
+        COLUMNS = len(board[0])
 
-        def isValidRow(rowIndex):
+        def checkRow(rowIndex):
             seen = set()
-            for c in range(COLS):
+            for c in range(COLUMNS):
                 if board[rowIndex][c] == '.':
                     continue
                 if board[rowIndex][c] in seen:
-                    return False
+                    return False 
                 seen.add(board[rowIndex][c])
             return True
-        
-        for row in range(ROWS):
-            if not isValidRow(row):
-                return False
 
-        def isValidColumn(colIndex):
+        for r in range(ROWS):
+            if not checkRow(r):
+                return False
+        
+        def checkColumn(colIndex):
             seen = set()
             for r in range(ROWS):
                 if board[r][colIndex] == '.':
@@ -28,11 +27,11 @@ class Solution:
                 seen.add(board[r][colIndex])
             return True
         
-        for col in range(COLS):
-            if not isValidColumn(col):
+        for c in range(COLUMNS):
+            if not checkColumn(c):
                 return False
 
-        def isValidSquare(rowIndex, colIndex): 
+        def checkSquare(rowIndex, colIndex):
             seen = set()
             for r in range(rowIndex, rowIndex + 3):
                 for c in range(colIndex, colIndex + 3):
@@ -42,10 +41,10 @@ class Solution:
                         return False
                     seen.add(board[r][c])
             return True
-        
+
         for r in range(0,9,3):
             for c in range(0,9,3):
-                if not isValidSquare(r,c):
+                if not checkSquare(r,c):
                     return False
 
         return True
