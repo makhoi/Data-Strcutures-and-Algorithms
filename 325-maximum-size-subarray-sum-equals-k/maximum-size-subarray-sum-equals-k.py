9dd078d548fa68b2ba78f58e1727d59c03ce4fab
Khoi Ma <khoihma@gmail.com>
2026-05-02 21:34:47 -0400

    Time: 146 ms (67.97%) | Memory: 67.5 MB (29.26%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
@@ -1,23 +1,26 @@
 class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
+        '''
+        ps[i] = s
+        ps[j->i] = k
+        ps[j-1] = s - k
+        '''
         res = 0
         prefix_index = {}
         sum = 0
-        jM1 = 0
 
         for i in range(len(nums)):
             sum += nums[i]
 
             if sum == k:
                 res = max(res, i + 1)
-
+            
             if sum - k in prefix_index:
                 jM1 = prefix_index[sum-k]
                 j = jM1 + 1
-                length = i - j + 1
-                res = max(res, length)
-
+                res = max(res, i - j + 1)
+            
             if sum not in prefix_index:
                 prefix_index[sum] = i
-            
+
         return res
