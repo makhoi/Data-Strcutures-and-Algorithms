42daae324fac61a34b8de929a505217d3aed87b9
Khoi Ma <khoihma@gmail.com>
2026-05-05 14:44:47 -0400

    Time: 246 ms (5.25%) | Memory: 20.5 MB (7.18%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -5,11 +5,9 @@ class Solution:
             return -1
 
         distances = []
-        total = 0
         for i in range(n):
             for j in range(i + 1, n):
                 for k in range(j + 1, n):
                     if nums[i] == nums[j] == nums[k]:
-                        total = abs(i-j) + abs(j-k) + abs(k-i)
-                        distances.append(total)
+                        distances.append(abs(i-j) + abs(j-k) + abs(k-i))
         return min(distances) if len(distances) else -1
