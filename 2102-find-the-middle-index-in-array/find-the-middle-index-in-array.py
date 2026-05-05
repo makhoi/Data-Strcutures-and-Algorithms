e6e30c9047146a642a39e4382cd90169560a54b2
Khoi Ma <khoihma@gmail.com>
2026-05-05 13:26:29 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (57.69%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.py
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.py
@@ -3,13 +3,16 @@ class Solution:
         n = len(nums)
         prefix_sum = [0]*n
         prefix_sum[0] = nums[0]
-        for i in range(1, len(nums)):
+        for i in range(1, n):
             prefix_sum[i] = prefix_sum[i-1] + nums[i]
-        total = prefix_sum[n - 1]
+
+        def leftSum(m):
+            return prefix_sum[m-1] if i else 0
+
+        def rightSum(m):
+            return prefix_sum[n-1] - prefix_sum[m]
 
         for i in range(n):
-            right_sum = total - prefix_sum[i] if i != n - 1 else 0
-            left_sum = prefix_sum[i-1] if i else 0
-            if right_sum == left_sum:
+            if leftSum(i) == rightSum(i):
                 return i
         return -1
