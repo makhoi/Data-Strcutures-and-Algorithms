35d8c847517112ab5a253f36c6edc96742027c13
Khoi Ma <khoihma@gmail.com>
2026-05-06 22:41:52 -0400

    Time: 158 ms (11.04%) | Memory: 43.8 MB (5.90%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -1,45 +1,27 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        '''
-        sum of array after removal operations = original total - x 
-        find the max length of the subarray that has k = original total - x 
-
-        ps[i] = S 
-        ps[j,i] = k = original - x
-        ps[j-1] = S - k
-
-        instead of return max_length, here we returns min k 
-        '''
-        prefix_sum = 0
-        prefix_map = {}
-        total = 0
-        max_length = 0
         n = len(nums)
-        min_operation = 0
+        max_length = 0
+        total = sum(nums)
+        length = len(nums)
+        s = 0
+        s_index = {}
 
-        for i in range(n):
-            total += nums[i]
-
-        k = total - x
-
-        if k == 0:
+        if total == x:
             return len(nums)
 
         for i in range(n):
-            prefix_sum += nums[i]
+            s += nums[i]
 
-            if prefix_sum == k:
+            if s == total - x:
                 max_length = max(max_length, i + 1)
 
-            if prefix_sum - k in prefix_map:
-                jM1 = prefix_map[prefix_sum-k]
+            if s - (total - x) in s_index:
+                jM1 = s_index[s-(total-x)]
                 j = jM1 + 1
-                l = i - j +1
-                max_length = max(max_length, l)
+                max_length = max(max_length, i - j + 1)
 
-            if prefix_sum not in prefix_map:
-                prefix_map[prefix_sum] = i
+            if s not in s_index:
+                s_index[s] = i
 
-            min_operation = n - max_length
-
-        return min_operation if max_length else -1
+        return (length - max_length) if max_length else -1
