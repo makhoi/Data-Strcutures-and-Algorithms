a23ed2f76fd3d40447a9ef691f9f8e8a0327258a
Khoi Ma <khoihma@gmail.com>
2026-05-08 13:14:42 -0400

    Time: 179 ms (74.84%) | Memory: 26 MB (12.73%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,19 +1,22 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        '''
+        return maximum length of subarray that has at most 2 different integers 
+        --> condition to stop the expansion: len(number_frequency) > 2
+        '''
         res = 0
-        integer_frequency = {}
+        number_frequency = {}
         left = 0
 
         for right in range(len(fruits)):
+            number_frequency[fruits[right]] = number_frequency.get(fruits[right], 0) + 1
 
-            while len(integer_frequency) == 2 and fruits[right] not in integer_frequency:
-                integer_frequency[fruits[left]] -= 1
-                if integer_frequency[fruits[left]] == 0:
-                    del integer_frequency[fruits[left]]
+            while len(number_frequency) > 2:
+                number_frequency[fruits[left]] -= 1
+                if number_frequency[fruits[left]] == 0:
+                    del number_frequency[fruits[left]]
                 left += 1
             
-            integer_frequency[fruits[right]] = integer_frequency.get(fruits[right], 0) + 1
-
             res = max(res, right - left + 1)
-
+        
         return res
