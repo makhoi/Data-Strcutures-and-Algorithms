e897f79b299221c2ce1ded7cf9ef376511c03d77
Khoi Ma <khoihma@gmail.com>
2026-05-09 16:46:37 -0400

    Time: 63 ms (77.30%) | Memory: 19.7 MB (37.07%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,35 +1,41 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        
         if t == "":
             return ""
 
-        countT, window = {}, {}
+        t_count, window = {}, {}
 
-        for c in t:
-            countT[c] = 1 + countT.get(c, 0)
+        for ch in t:
+            t_count[ch] = t_count.get(ch, 0) + 1
 
-        have, need = 0, len(countT)
-        res, resLen = [-1, -1], float("infinity")
-        l = 0
+        for ch in t:
+            if ch not in window:
+                window[ch] = 0
+        
+        have, need = 0, len(t_count)
 
-        for r in range(len(s)):
-            c = s[r]
-            window[c] = 1 + window.get(c, 0)
+        min_len = float('inf')
+        current_len = 0
+        res = [-1, -1]
+        left = 0
 
-            if c in countT and window[c] == countT[c]:
-                have += 1
-
+        for right in range(len(s)):
+            if s[right] in window:
+                window[s[right]] += 1
+                if window[s[right]] == t_count[s[right]]:
+                    have += 1
+            
             while have == need:
-                # update our result
-                if (r - l + 1) < resLen:
-                    res = [l, r]
-                    resLen = r - l + 1
-
-                # pop from the left of our window
-                window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
-                    have -= 1
-                l += 1
+                current_len = right - left + 1
+                if current_len < min_len:
+                    res = [left, right]
+                    min_len = current_len
+                if s[left] in window:
+                    window[s[left]] -= 1
+                    if window[s[left]] < t_count[s[left]]:
+                        have -= 1
+                left += 1
 
         l, r = res
-        return s[l:r + 1] if resLen != float("infinity") else ""
+        return s[l:r+1] if res != float('inf') else ""
