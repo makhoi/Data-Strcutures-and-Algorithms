6f01e67fcefaa74e3745b2b328a2ba6e48d941d9
Khoi Ma <khoihma@gmail.com>
2026-05-11 13:07:04 -0400

    Time: 162 ms (74.62%) | Memory: 19.6 MB (94.25%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
@@ -1,19 +1,24 @@
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        '''
+        contain at most 2 distinct character -> condition to stop the expansion: len(character_frequency) > 2
+        '''
         res = 0
         character_frequency = {}
         left = 0
 
         for right in range(len(s)):
+            character = s[right]
+            character_frequency[character] = character_frequency.get(character, 0) + 1
+            distinct = len(character_frequency)
 
-            while len(character_frequency) == 2 and s[right] not in character_frequency:
+            while distinct > 2:
                 character_frequency[s[left]] -= 1
                 if character_frequency[s[left]] == 0:
                     del character_frequency[s[left]]
+                    distinct -= 1
                 left += 1
-            
-            character_frequency[s[right]] = character_frequency.get(s[right], 0) + 1
 
             res = max(res, right - left + 1)
-            
+
         return res
