2ac4a36e489ebe7119b0b90a6a225ac96925c031
Khoi Ma <khoihma@gmail.com>
2026-05-15 01:21:08 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (20.51%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.py
@@ -1,7 +1,21 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
-        count = 0
-        for num in nums:
-            if num < k:
-                count += 1
-        return count
+        '''
+        solve this using binary search:
+        - sort the array
+        - find the first appearance that is larger or equal to k
+        '''
+        nums = sorted(nums)
+
+        n = len(nums)
+        left = 0
+        right = n - 1
+
+        while left < right:
+            mid = left + (right - left)//2
+
+            if nums[mid] >= k:
+                right = mid
+            else:
+                left = mid + 1
+        return left
