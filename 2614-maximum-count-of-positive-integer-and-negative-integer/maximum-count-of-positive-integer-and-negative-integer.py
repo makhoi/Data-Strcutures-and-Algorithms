127acc34d72126c0dd79751416afd262125c74ba
Khoi Ma <khoihma@gmail.com>
2026-05-15 01:08:48 -0400

    Time: 0 ms (100.00%) | Memory: 19.6 MB (18.25%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,11 +1,47 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-        pos = 0
-        neg = 0
+        '''
+        in case there is no zero
+        use binary search -> find first appearance of positive integer
+        -> count of pos = len(nums) - left
+        -> count of neg = left
 
-        for num in nums:
-            if num < 0: 
-                neg += 1
-            if num > 0:
-                pos += 1
+        in case there are zeros:
+        use lowerbound binary search to find first appearance of positive integer
+        -> pos = len(nums) - left
+        use upperbound bs to find last appearance of negative integer
+        -> neg = right
+        O(logn)
+        '''
+        n = len(nums)
+        left = 0
+        right = n - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] > 0:
+                right = mid
+            else:
+                left = mid + 1
+        if nums[left] <= 0:
+            pos = 0
+        else:
+            pos = n - left
+
+        left = 0
+        right = n - 1
+
+        while left < right:
+            mid = right - (right - left) // 2
+
+            if nums[mid] < 0:
+                left = mid
+            else:
+                right = mid - 1
+        if nums[right] >= 0:
+            neg = 0
+        else:
+            neg = right + 1
+
         return max(pos, neg)
