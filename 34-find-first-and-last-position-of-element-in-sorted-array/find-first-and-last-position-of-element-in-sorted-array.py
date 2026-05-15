08ca957a3412abdce38c849fd4c9b64ae04f6eac
Khoi Ma <khoihma@gmail.com>
2026-05-14 22:41:45 -0400

    Time: 0 ms (100.00%) | Memory: 20.5 MB (90.47%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,52 +1,34 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         '''
-        1. Define approach:
-        - search space: index increases from 0 -> len(nums) - 1
-        - Consecutiveness
-        => Binary Search problem
-
-        2. Decide lowerbound vs upperbound: asking for both earlier appaearance and latter appearance 
-        => lowerbound and upperbound
-
-        3. Define search space: 
-        index 0 -> len(nums) - 1
-
-        4. Define condition(mid): 
-        lowerbound: target <= nums[mid] -> right = mid
-        upperbound: target >= nums[mid] -> left = mid
-
-        5. Define meaning of left and right:
-        left: min left satifies lowerbound condition
-        right: max right satisfies upperbound condition
+        find first element -> lowerbound 
+        find last element -> upperbound
         '''
-
-        n = len(nums)
-        left = 0
-        right = n - 1
-
-        if n == 0:
+        if not nums:
             return [-1, -1]
 
+        left = 0
+        right = len(nums) - 1
+
         while left < right:
-            mid = left + (right - left) // 2
+            mid = left + (right - left)//2
 
             if target <= nums[mid]:
                 right = mid
             else:
                 left = mid + 1
-
+        
         leftRes = left
 
         left = 0
-        right = n - 1
+        right = len(nums) - 1
 
         while left < right:
-            mid = right - (right-left)//2
+            mid = right - (right - left) // 2
 
             if target >= nums[mid]:
                 left = mid
             else:
                 right = mid - 1
-        
-        return [-1,-1] if nums[leftRes] != target and nums[right] != target else [leftRes, right]
+
+        return [leftRes, right] if nums[leftRes] == target and nums[right] == target else [-1,-1]
