eaa1085e38616e4d22d3cd49492fc0e4c4a23d4c
Khoi Ma <khoihma@gmail.com>
2026-05-16 17:32:32 -0400

    Time: 191 ms (25.80%) | Memory: 20.5 MB (80.85%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,23 +1,22 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         
-        def canEatWithinHHours(k):
+        def can_eat_within_h(k):
             total = 0
             for pile in piles:
-                total += pile // k 
+                total += pile // k
                 if pile % k != 0:
                     total += 1
             return total <= h
-
+        
         left = 1
         right = max(piles)
 
         while left < right:
             mid = left + (right - left) // 2
 
-            if canEatWithinHHours(mid):
+            if can_eat_within_h(mid):
                 right = mid
             else:
                 left = mid + 1
-        
         return left
