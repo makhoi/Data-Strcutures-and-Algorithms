837227ff2fb39a026430de748449cb85fac1a573
Khoi Ma <khoihma@gmail.com>
2026-05-18 20:53:44 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (57.42%) - LeetSync

--- 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.py
+++ 4264-first-matching-character-from-both-ends/first-matching-character-from-both-ends.py
@@ -1,7 +1,13 @@
 class Solution:
     def firstMatchingIndex(self, s: str) -> int:
         n = len(s)
-        for i in range(n):
-            if s[i] == s[n-i-1]:
-                return i
+        left = 0
+        right = n - 1
+
+        while left <= right: 
+            if s[left] == s[right]:
+                return left
+            left += 1
+            right -= 1
+
         return -1
