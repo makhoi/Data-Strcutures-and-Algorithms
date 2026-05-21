256083e8dabd663eb0f5add2b4cac4dc3113b133
Khoi Ma <khoihma@gmail.com>
2026-05-21 12:43:24 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (55.80%) - LeetSync

--- 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.py
+++ 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.py
@@ -1,10 +1,5 @@
 class Solution:
     def maximizeExpressionOfThree(self, nums: List[int]) -> int:
-        '''
-        maximize = a and b are 2 largest values and c is smallest value 9 in case this is all positive values)
-        if there is negative:
-            c is the smallest negative value
-        '''
         smallest = min(nums)
 
         n = sorted(nums)
