dfd95d592863c0d36c2d8dcae1a3b1345d7fac4e
Khoi Ma <khoihma@gmail.com>
2026-05-21 12:17:23 -0400

    Time: 2 ms (41.38%) | Memory: 19.3 MB (81.70%) - LeetSync

--- 4126-minimum-number-of-flips-to-reverse-binary-string/minimum-number-of-flips-to-reverse-binary-string.py
+++ 4126-minimum-number-of-flips-to-reverse-binary-string/minimum-number-of-flips-to-reverse-binary-string.py
@@ -1,22 +1,16 @@
 class Solution:
     def minimumFlips(self, n: int) -> int:
-        '''
-        Convert number to binary number
-        Determine its reverse version
-        Convert that reverse version in binary number to integer number
-        Compare between original binary num and reversed binary num, one by one element -> if different add 1 to count
-        '''
-        binary_num = f"{n:b}"
-
-        reverse_binary_array = []
-        for i in range(len(binary_num)-1, -1, -1):
-            reverse_binary_array.append(binary_num[i])
-        reversed_binary_num = "".join(reverse_binary_array)
         
-        flip_count = 0
-        for i in range(len(binary_num)):
-            if binary_num[i] != reverse_binary_array[i]:
-                flip_count += 1
-        return flip_count
+        binary = bin(n)[2:]
 
+        reverse = []
+        for i in range(len(binary) - 1, -1, -1):
+            reverse.append(binary[i])
+        reverse = "".join(reverse)
 
+        res = 0
+        for i in range(len(binary)):
+            if binary[i] != reverse[i]:
+                res += 1
+                
+        return res
