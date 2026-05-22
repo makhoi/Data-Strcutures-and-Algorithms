2a6adf9e99c701198cc09a0fad39314412ad4a1a
Khoi Ma <khoihma@gmail.com>
2026-05-22 13:04:40 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (67.29%) - LeetSync

--- 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.py
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.py
@@ -5,13 +5,9 @@ class Solution:
         for num in nums:
             num_frequency[num] = num_frequency.get(num, 0) + 1
 
-        frequency_divisible_k = []
+        res = 0
         for num, frequency in num_frequency.items():
             if frequency % k == 0:
-                frequency_divisible_k.append(num)
-
-        res = 0
-        for num in frequency_divisible_k:
-            res += num*num_frequency[num]
-        
+                res += num*frequency
+                
         return res
