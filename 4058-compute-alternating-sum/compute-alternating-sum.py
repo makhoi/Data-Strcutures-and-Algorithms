d369a68ed13b2c1eda71f242c11c975299177172
Khoi Ma <khoihma@gmail.com>
2026-05-22 14:06:58 -0400

    Time: 4 ms (16.62%) | Memory: 19.3 MB (16.46%) - LeetSync

--- 4058-compute-alternating-sum/compute-alternating-sum.py
+++ 4058-compute-alternating-sum/compute-alternating-sum.py
@@ -1,6 +1,7 @@
 class Solution:
-    def alternatingSum(self, nums: List[int]) -> int:
-        res = 0
-        for i, num in enumerate(nums):
-            res += round(math.cos(math.pi * i) * num)
-        return res
+    def alternatingSum(self, nums: List[int], ans = 0) -> int:
+        n = len(nums)
+        for i in range(0, n, 2): ans += nums[i]
+        for i in range(1, n, 2): ans -= nums[i]
+
+        return ans
