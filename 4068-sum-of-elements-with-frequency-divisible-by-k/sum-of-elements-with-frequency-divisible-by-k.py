9f0557ff282ca410f7638f4133cab081552a16ac
Khoi Ma <khoihma@gmail.com>
2026-05-22 13:09:53 -0400

    Time: 3 ms (33.41%) | Memory: 19.3 MB (27.61%) - LeetSync

--- 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.py
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/sum-of-elements-with-frequency-divisible-by-k.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 class Solution:
     def sumDivisibleByK(self, nums: List[int], k: int) -> int:
         return sum(num * frequency for num, frequency in Counter(nums).items() if frequency % k == 0)
