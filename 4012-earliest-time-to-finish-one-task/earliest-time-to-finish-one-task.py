07564c82ff7074a24535f6f7c0384c80a6b69475
Khoi Ma <khoihma@gmail.com>
2026-05-25 16:47:55 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (17.72%) - LeetSync

--- 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.py
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.py
@@ -1,8 +1,6 @@
 class Solution:
     def earliestTime(self, tasks: List[List[int]]) -> int:
-        total_time = 0
-        min_time = float('inf')
-        for i in range(len(tasks)):
-            total_time = sum(tasks[i])
-            min_time = min(min_time, total_time)
-        return min_time
+        res = float('inf')
+        for task in tasks:
+            res = min(res, task[0] + task[1])
+        return res
