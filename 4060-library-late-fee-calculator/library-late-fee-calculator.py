61a53abcfd51db439af0c7f5abaf66d1030ef26f
Khoi Ma <khoihma@gmail.com>
2026-05-25 16:22:16 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.33%) - LeetSync

--- 4060-library-late-fee-calculator/library-late-fee-calculator.py
+++ 4060-library-late-fee-calculator/library-late-fee-calculator.py
@@ -1,12 +1,11 @@
 class Solution:
     def lateFee(self, daysLate: List[int]) -> int:
-        late_fee = 0
+        total = 0
         for day in daysLate:
             if day == 1:
-                late_fee += 1
-            elif day >= 2 and day <= 5:
-                late_fee += day * 2
+                total += 1
+            elif 2 <= day <= 5:
+                total += 2*day
             else:
-                late_fee += day * 3
-
-        return late_fee
+                total += 3*day
+        return total
