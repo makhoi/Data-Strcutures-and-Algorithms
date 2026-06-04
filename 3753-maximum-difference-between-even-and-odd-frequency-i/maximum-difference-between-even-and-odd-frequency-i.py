7a36bdfbf92bff73707ea93b01e75bd82942b8c4
Khoi Ma <khoihma@gmail.com>
2026-06-04 01:23:42 -0400

    Time: 3 ms (62.53%) | Memory: 19.1 MB (91.56%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.py
@@ -1,35 +1,16 @@
 class Solution:
     def maxDifference(self, s: str) -> int:
-        '''
-        Find the maximum difference of odd and even frequency 
-        -> max difference = max_frequency_odd - min_frequency_even 
-        using 3 hashmaps
-        first hashmap character_frequency to track frequency of the character_frequency
-        second hashmap odd_frequency to store only character with odd frequency from the first hashmap 
-        third hashmap even_frequency to store only character with even frequency from the first hashmap
-        pick the key with max value from second table and key with min value from third table 
-        max_diff = max(odd) - min(even)
-        '''
         character_frequency = {}
         for ch in s:
             character_frequency[ch] = character_frequency.get(ch, 0) + 1
 
-        odd_frequency = {}
-        max_odd_frequency = 0
-        for ch, frequency in character_frequency.items():
-            if frequency % 2 == 1:
-                odd_frequency[ch] = frequency
-                if frequency > max_odd_frequency:
-                    max_odd_frequency = frequency
+        max_odd = float('-inf')
+        min_even = float('inf')
+        for frequency in character_frequency.values():
+            if frequency % 2 == 1 and frequency > max_odd:
+                max_odd = frequency
 
-        even_frequency = {}
-        min_even_frequency = float('inf')
-        for ch, frequency in character_frequency.items():
-            if frequency % 2 == 0:
-                even_frequency[ch] = frequency
-                if frequency < min_even_frequency:
-                    min_even_frequency = frequency
+            elif frequency % 2 == 0 and frequency < min_even:
+                min_even = frequency
 
-        diff = max_odd_frequency - min_even_frequency
-
-        return diff
+        return max_odd - min_even
