f68b41960293685b79f4617ac4fc6e39a7218460
Khoi Ma <khoihma@gmail.com>
2026-06-04 02:00:45 -0400

    Time: 4 ms (31.04%) | Memory: 19.3 MB (73.13%) - LeetSync

--- 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.py
+++ 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.py
@@ -1,17 +1,18 @@
 class Solution:
     def findValidPair(self, s: str) -> str:
+        nums = []
+        for ch in s:
+            nums.append(int(ch))
+
         number_frequency = {}
-        for n in s:
-            number_frequency[n] = number_frequency.get(n, 0) + 1
+        for num in nums:
+            number_frequency[num] = number_frequency.get(num, 0) + 1
 
         valid_pair = []
-        for i in range(len(s)-1):
-            if s[i] == s[i+1]:
-                continue
-            else: 
-                if number_frequency[s[i]] == int(s[i]) and number_frequency[s[i+1]] == int(s[i+1]):
-                    valid_pair.append(s[i])
-                    valid_pair.append(s[i+1])
-                    break
+        for i in range(len(nums) - 1):
+            if nums[i] != nums[i+1] and nums[i] == number_frequency[nums[i]] and nums[i+1] == number_frequency[nums[i+1]]:
+                valid_pair.append(str(nums[i]))
+                valid_pair.append(str(nums[i+1]))
+                break
 
-        return "".join(valid_pair)
+        return "".join(valid_pair) if valid_pair else ""
