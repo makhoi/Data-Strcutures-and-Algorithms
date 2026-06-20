7c217a59fba08c63cdb63b02217f4c3ba32f43ce
Khoi Ma <khoihma@gmail.com>
2026-06-20 01:26:37 -0400

    Time: 64 ms (45.87%) | Memory: 19.3 MB (13.44%) - LeetSync

--- 3651-transformed-array/transformed-array.py
+++ 3651-transformed-array/transformed-array.py
@@ -1,31 +1,8 @@
 class Solution:
-    def constructTransformedArray(self, nums: List[int]) -> List[int]:
-        result = [0]*len(nums)
-
-        last_index = len(nums) - 1
-
-        for current_index in range(len(nums)):
-            step = nums[current_index]
-
-            if step < 0: 
-                # next_index = current_index - abs(step)
-                # if next_index < 0: 
-                #     next_index = abs(step) - current_index - 1 + last_index
-                # next_value = nums[next_index]
-                # result[current_index] = next_value
-                result[current_index] = nums[(current_index +nums[current_index])%len(nums)]
-
-            elif step > 0: 
-                # next_index = current_index + step
-                # if next_index > last_index:
-                #     next_index = current_index + step - last_index - 1
-                # next_value = nums[next_index]
-                # result[current_index] = next_value
-                result[current_index] = nums[(current_index +nums[current_index])%len(nums)]
-
-            else: 
-                next_index = current_index
-                next_value = nums[next_index]
-                result[current_index] = next_value
-
-        return result
+    def constructTransformedArray(self, nums):
+        '''
+        The basic idea for the given formula is as follows:
+        We need to start at i and move nums[i] right or left and this is denoted in the sign of i + nums[i]. We modulo it by the length len to make sure it stays within the size of the given vector, but if nums[i] is a negative number, it's possible that the result is negative. Therefore, we can add len to our result and then modulo len again, effectively doing the same modulo operation that we just did. Adding len will ensure that our result is positive, and it's mathematically valid because adding by the length of the vector puts us in the same spot (it's like walking around whole array back to where we are). Thus, we can just modulo that and get to where we need to be. Hence, ((i + nums[i]) % len + len) % len
+        '''
+        n = len(nums)
+        return [nums[((i + nums[i]) % n + n) % n] for i in range(n)]
